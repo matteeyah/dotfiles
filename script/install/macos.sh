7e3fda6887ce7e20847a1493fe6bae74a3bef74d
Matija Čupić <matija.cupic.97@gmail.com>
2017-06-13 04:26:28 +0200

    Removed python config

--- script/install/macos.sh
+++ script/install/macos.sh
@@ -4,9 +4,7 @@
 brew install tig
 brew install zsh
 brew install rbenv
-brew install pyenv
 brew install vim
 
 brew cask install font-fira-code
 brew cask install docker
-brew cask install atom
--- script/python.sh
+++
@@ -1,5 +0,0 @@
-# Install python
-pyenv install 3.6.1
-
-pip3 install mypy
-pip3 install pylint
