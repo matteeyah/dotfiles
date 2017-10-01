13f177fc1628af574d7048d7a79a223ce971e402
Matija Čupić <matija.cupic.97@gmail.com>
2017-10-01 17:49:13 +0200

    Add python to install script

--- script/install/debian.sh
+++ script/install/debian.sh
@@ -7,9 +7,16 @@ sudo apt -y install tig
 sudo apt -y install zsh
 sudo apt -y install vim
 sudo apt -y install rbenv
+sudo apt -y install pyenv
 # Install the ruby-build plugin for rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+# Install the pyenv-virtualenvwrapper plugin for pyenv
+git clone https://github.com/pyenv/pyenv-virtualenvwrapper.git $(pyenv root)/plugins/pyenv-virtualenvwrapper
+
 sudo apt -y install curl
 
 # Install ruby dependencies
 sudo apt -y install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+
+# Install python dependencies
+sudo apt -y install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
---
+++ script/python.sh
@@ -0,0 +1,4 @@
+# Install python
+pyenv install 3.6.2
+
+pyenv global 3.6.2
