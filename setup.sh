2f7a88bb26ad587e85f1c9e9a5304ffcd088fc92
Matija Čupić <matija.cupic.97@gmail.com>
2017-10-10 21:57:27 +0200

    Refactor setup stages into functions
    * Refactor setup stages into functions
    * Parse flags for each stage

--- setup.sh
+++ setup.sh
@@ -1,25 +1,69 @@
-#!/bin/sh
-
-# Update submodules
-echo "Updating submodules..."
-git submodule update --init --recursive
+#!/bin/bash
 
 # Prune symlinks
-echo "Pruning symlinks..."
-find ~ -maxdepth 1 -type l -delete
+clean() {
+    echo "Pruning symlinks..."
+    find ~ -maxdepth 1 -type l -delete
+}
+
+# Update submodules
+update() {
+    echo "Updating submodules..."
+    git submodule update --init --recursive
+}
 
 # Create config symlinks
-echo "Symlinking config files..."
-ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
-ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+symlink() {
+    echo "Symlinking config files..."
+    ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
+    ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+    ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+}
 
 # Install stuff
-echo "Installing packages..."
-sh ./script/install.sh
-echo "Installing ruby..."
-sh ./script/ruby.sh
-echo "Installign python..."
-sh ./script/python.sh
-echo "Installing vim plugins..."
-sh ./script/vim.sh
+install() {
+    echo "Installing packages..."
+    sh ./script/install.sh
+    echo "Installing ruby..."
+    sh ./script/ruby.sh
+    echo "Installign python..."
+    sh ./script/python.sh
+    echo "Installing vim plugins..."
+    sh ./script/vim.sh
+}
+
+usage() {
+  cat << EOF
+usage: $0 [-cusih]
+-c     clean symlinks
+-u     update dotfiles repo
+-s     symlink config files
+-i     install system packages
+-h     show this
+EOF
+}
+
+[ $# -eq 0 ] && usage
+while getopts "cusih" opt; do
+  case $opt in
+    c)
+      clean
+      ;;
+    u)
+      update
+      ;;
+    s)
+      symlink
+      ;;
+    i)
+      install
+      ;;
+    h | *)
+      usage
+      exit 0
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+[ $# -ne 0 ] && usage
